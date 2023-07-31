960fdf966b81f87f695079571435f4aa552c7f43
Dorson Williams <dorson@live.com>
2023-07-31 00:32:17 -0400

    switched to google translator

--- machinetranslation/translator.py
+++ machinetranslation/translator.py
@@ -1,49 +1,17 @@
-#Creates a Language Translator Service between French and English.
-
 import os
-from ibm_watson import LanguageTranslatorV3
-from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from dotenv import load_dotenv
+from deep_translator import GoogleTranslator
 
-load_dotenv()
-
-apikey = os.environ['apikey']
-url = os.environ['url']
-
-authenticator = IAMAuthenticator(apikey)
-language_translator = LanguageTranslatorV3(
-    version='2022-07-18',
-    authenticator=authenticator
-)
-
-language_translator.set_service_url(url)
-
-def english_to_french(english_text):
+def english_to_french(text):
     #Receives text in English and returns its French translation.
+    my_translator = GoogleTranslator(source='auto', target='german')
+    result = my_translator.translate(text=text)
+    print(f"Translation using source = {my_translator.source} and target = {my_translator.target} -> {result}")
+    return result
 
-    french_translation = language_translator.translate(
-        text=english_text,
-        model_id='en-fr'
-    ).get_result()
-    french_text = french_translation.get("translations")[0].get("translation")
-    return french_text
-
-def english_to_german(english_text):
+def english_to_german(text):
     #Receives text in English and returns its German translation.
-
-    german_translation = language_translator.translate(
-        text=english_text,
-        model_id='en-de'
-    ).get_result()
-    german_text = german_translation.get("translations")[0].get("translation")
-    return german_text
-
-def french_to_english(french_text):
-    #Receives a text in French and returns its English translation.
-
-    english_translation = language_translator.translate(
-        text=french_text,
-        model_id='fr-en'
-    ).get_result()
-    english_text = english_translation.get("translations")[0].get("translation")
-    return english_text
+    my_translator = GoogleTranslator(source='auto', target='french')
+    result = my_translator.translate(text=text)
+    print(f"Translation using source = {my_translator.source} and target = {my_translator.target} -> {result}")
+    return result
