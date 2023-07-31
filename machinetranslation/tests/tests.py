b790e459d51e291b8416b5a2561a68df69b6b8ab
Dorson Williams <dorson@live.com>
2023-07-31 02:18:28 -0400

    idk

--- machinetranslation/tests/tests.py
+++ machinetranslation/tests/tests.py
@@ -1,17 +1,19 @@
-import unittest
 import sys
-sys.path.append("machinetranslation")
 import translator
-from translator import french_to_english, english_to_french
+from translator import english_to_german, english_to_french
+import unittest
+sys.path.append("machinetranslation")
 
 
 class TestTranslator(unittest.TestCase):
-
-    def test_english_to_french(self):
+   '''idk'''
+def test_english_to_french(self):
+        '''en to fr'''
         self.assertEqual(english_to_french('Hello'), 'Bonjour')
         self.assertNotEqual(english_to_french("None"), '')
     
-    def test_english_to_german(self):
+def test_english_to_german(self):
+        '''en to de'''
         self.assertEqual(english_to_german('Hello'), 'Hallo')
         self.assertNotEqual(english_to_german("None"), '')
 
