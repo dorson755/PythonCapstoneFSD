55bc41fdfe9a0690010da6d9dbd4f37cf5c1eb49
Dorson Williams <68614535+dorson755@users.noreply.github.com>
2023-08-04 19:30:34 +0000

    code shape up

--- machinetranslation/tests/test_translator.py
+++ machinetranslation/tests/test_translator.py
@@ -1,21 +1,27 @@
+"""
+Module Name: test_translator.py
+Description: This is the test module for translator.py.
+
+Author: Dorson Williams
+Date: August 2023
+"""
+
 import unittest
-import sys
-import translator
-from translator import english_to_german, english_to_french
-sys.path.append("machinetranslation")
+from machinetranslation.translator import english_to_german, english_to_french
+#sys.path.append("machinetranslation")
 
 
 class TestTranslator(unittest.TestCase):
-   '''idk'''
+    '''class for unit test'''
 def test_english_to_french(self):
-        '''en to fr'''
-        self.assertEqual(english_to_french('Hello'), 'Bonjour')
-        self.assertNotEqual(english_to_french("None"), '')
-    
+    '''en to fr'''
+    self.assertEqual(english_to_french('Hello'), 'Bonjour')
+    #self.assertNotEqual(english_to_french("None"), '')
+
 def test_english_to_german(self):
-        '''en to de'''
-        self.assertEqual(english_to_german('Hello'), 'Hallo')
-        self.assertNotEqual(english_to_german("None"), '')
+    '''en to de'''
+    self.assertEqual(english_to_german('Hello'), 'Hallo')
+    #self.assertNotEqual(english_to_german("None"), '')
 
 if __name__ == '__main__':
     unittest.main()
